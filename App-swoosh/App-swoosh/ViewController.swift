233679c1008e243d3cd73a9033776126111ccec0
Danny Bokati <dannybokati@gmail.com>
2017-12-04 14:05:37 +0545

    new commit

--- App-swoosh/App-swoosh/ViewController.swift
+++ App-swoosh/App-swoosh/ViewController.swift
@@ -20,6 +20,10 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func unwindFromSkillVC(unwindsegue: UIStoryboardSegue){
+        
+    }
 
 
 }
